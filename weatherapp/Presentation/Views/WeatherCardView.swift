256bb58f176e072a28446c841bd768e0d00e56d7
Sanket Pimple <sanketpimple@gmail.com>
2023-03-07 19:39:53 -0800

    Add UI - containerView and viewController changes

--- weatherapp/Presentation/Views/WeatherCardView.swift
+++ weatherapp/Presentation/Views/WeatherCardView.swift
@@ -24,9 +24,135 @@ class WeatherCardView: UIView {
     init(with weatherViewModel: WeatherViewModel) {
         self.weatherViewModel = weatherViewModel
         super.init(frame: .zero)
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+//TODO: separate out every component in its own function
+//        TODO: set font, color etc
+//        TODO: find out best way to set data to the labels
+        
+        let mainStackView = UIStackView()
+        mainStackView.axis = .horizontal
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.distribution = .equalCentering
+        mainStackView.alignment = .center
+        mainStackView.spacing = UIStackView.spacingUseSystem
+        mainStackView.isLayoutMarginsRelativeArrangement = true
+        self.addSubview(mainStackView)
+        
+        self.backgroundColor = .systemGray5
+
+        //------Date Time------------
+//        self.addSubview(dateTimeLabel)
+        if let timeStamp = weatherViewModel.weatherObj.timeStamp {
+            dateTimeLabel.text = getDateTime(timestamp: timeStamp)
+        }
+        dateTimeLabel.textColor = .black
+        dateTimeLabel.sizeToFit()
+
+//        self.addSubview(temperatureLabel)
+        if let temperature = weatherViewModel.weatherObj.mainData.temperature {
+            temperatureLabel.text = String(temperature)
+        }
+        temperatureLabel.font = .systemFont(ofSize: 60, weight: .regular)
+        temperatureLabel.textColor = .black
+        temperatureLabel.sizeToFit()
+ 
+//        self.addSubview(feelsLikeLabel)
+        feelsLikeLabel.textColor = .black
+        feelsLikeLabel.sizeToFit()
+//        feelsLikeLabel.font = .systemFont(ofSize: 60, weight: .regular)
+        if let feelsLike = weatherViewModel.weatherObj.mainData.feelsLike {
+            feelsLikeLabel.text = String(feelsLike)
+        }
+
+//        self.addSubview(hightLowTempLabel)
+       hightLowTempLabel.textColor = .black
+       hightLowTempLabel.sizeToFit()
+//        hightLowTempLabel.font = .systemFont(ofSize: 60, weight: .regular)
+        
+        if let minTemp = weatherViewModel.weatherObj.mainData.minTemp, let maxTemp = weatherViewModel.weatherObj.mainData.maxTemp {
+            hightLowTempLabel.text = "H:\(String(maxTemp)) L:\(String(minTemp))"
+        }
+//        NSLayoutConstraint.activate([
+//            hightLowTempLabel.topAnchor.constraint(equalTo: feelsLikeLabel.bottomAnchor)])
+      
+        let infoStackView = UIStackView(arrangedSubviews: [dateTimeLabel, temperatureLabel, feelsLikeLabel, hightLowTempLabel])
+        infoStackView.axis = .vertical
+        infoStackView.translatesAutoresizingMaskIntoConstraints = false
+//        infoStackView.spacing = UIStackView.spacingUseSystem
+//        infoStackView.isLayoutMarginsRelativeArrangement = true
+//        infoStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+        mainStackView.addArrangedSubview(infoStackView)
+        
+        
+        //        ---------------------------image---------
+        if !weatherViewModel.weatherObj.weatherInfo.isEmpty {
+            let url = URL(string: "https://openweathermap.org/img/wn/\(weatherViewModel.weatherObj.weatherInfo[0].icon)@2x.png")
+
+            KF.url(url)
+    //          .placeholder(placeholderImage)
+    //          .setProcessor(processor)
+              .loadDiskFileSynchronously()
+              .fromMemoryCacheOrRefresh()
+              .onSuccess { result in
+                  print("image loaded successfully")
+              }
+              .onFailure { error in
+                  print("image loading error: \(error)")
+              }
+              .set(to: weatherIcon)
+            
+            weatherConditionLabel.textColor = .black
+            weatherConditionLabel.sizeToFit()
+            weatherConditionLabel.textAlignment = .center
+            weatherConditionLabel.text = weatherViewModel.weatherObj.weatherInfo[0].mainWeather
+        }
+        
+        
+       
+        
+        let imageStackView = UIStackView(arrangedSubviews: [weatherIcon, weatherConditionLabel])
+        imageStackView.axis = .vertical
+        imageStackView.alignment = .center
+        imageStackView.translatesAutoresizingMaskIntoConstraints = false
+//        imageStackView.spacing = UIStackView.spacingUseSystem
+//        imageStackView.isLayoutMarginsRelativeArrangement = true
+//        imageStackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+//        imageStackView.leadingAnchor.constraint(equalTo: infoStackView.trailingAnchor).isActive = true
+//        imageStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+        mainStackView.addArrangedSubview(imageStackView)
+//        
+        setShadowWithCornerRadius(corners: 16)
+    }
+    
+    func setShadowWithCornerRadius(corners : CGFloat){
+           self.layer.cornerRadius = corners
+           self.layer.masksToBounds = false
+           self.layer.shadowColor = UIColor.lightGray.cgColor
+            self.layer.shadowOffset = .zero
+           self.layer.shadowOpacity = 0.5
+           self.layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        self.layer.shouldRasterize = true
+        self.layer.shadowRadius = 10
+        self.layer.rasterizationScale = UIScreen.main.scale
+    
+           }
+    
+//    TODO: Move to utilities
+    func getDateTime(timestamp: Double) -> String {
+        
+        let date = Date(timeIntervalSince1970: timestamp)
+            let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = DateFormatter.Style.short //Set time style
+        dateFormatter.dateStyle = DateFormatter.Style.medium //Set date style
+            dateFormatter.timeZone = .current
+            let localDate = dateFormatter.string(from: date)
+        return localDate
     }
 }
--- weatherapp/Presentation/WeatherViewController.swift
+++ weatherapp/Presentation/WeatherViewController.swift
@@ -5,17 +5,21 @@
 //  Created by Sanket Pimple on 3/7/23.
 //
 
+import Foundation
 import UIKit
 import MapKit
 import CoreLocation
 
+//TODO: Remove all the print statements
 //TODO: Seaprate out uisearchbardelegate to an extension
+//TODO: Code formatting and indentation
 class WeatherViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDelegate {
 
     private var viewModel = WeatherViewModel()
     private var citiesList: [CityItem] = JSONParsing.parseCitiesJSON()
     private var isWeatherFetched: Bool = false
-    private var locationManager: CLLocationManager?
+    private var locationManager: CLLocationManager? // Ideally all location related code should be moved to a LocationService class. It can be made a singleton if required
+    let userDefaults = UserDefaults.standard
 
     lazy var searchController: UISearchController = {
         let searchController = UISearchController(searchResultsController: nil)
@@ -33,20 +37,106 @@ class WeatherViewController: UIViewController, UISearchBarDelegate, CLLocationMa
         
         locationManager = CLLocationManager()
         locationManager?.requestWhenInUseAuthorization()
+        
 //      TODO: get current location and send to fetchWeather
+//        if last location is stored, get that, else, show weather for current location
+        if let lastLocation = userDefaults.string(forKey: "last_location") {
+            fetchWeather(city: lastLocation)
+        } else {
+            // TODO: fetchWeather for current user location
+        }
 //        fetchWeather()
+        setupView()
+    }
+
+    func setupView() {
         view.backgroundColor = .systemBackground
         searchController.searchBar.searchTextField.placeholder = NSLocalizedString("Enter a search term", comment: "")
         searchController.searchBar.delegate = self
         navigationItem.searchController = searchController
+        
+//        let scrollView = UIScrollView(frame: .zero)
+//        view.addSubview(scrollView)
+//
+//        scrollView.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+//            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+//        ])
+//
+//        scrollView.backgroundColor = .systemGreen
+        
+        let stackView = UIStackView()
+//        scrollView.addSubview(stackView)
+        stackView.axis = .vertical
+//        stackView.distribution = .equalCentering
+        stackView.spacing = 20
+//        stackView.bounds = CGRectInset(stackView.frame, 10.0, 10.0)
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+//            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+//            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+//        ])
+
+        let card = WeatherCardView(with: viewModel)
+//        stackView.addArrangedSubview(card)
+        view.addSubview(card)
+
+        NSLayoutConstraint.activate([
+            card.widthAnchor.constraint(equalToConstant: 365),
+            card.heightAnchor.constraint(equalToConstant: 240),
+            card.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            card.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            card.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ])
+
+        card.translatesAutoresizingMaskIntoConstraints = false
+        
+//TODO: use uilabels and imageviews for now, rey scrollview and stackview later
+        
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let whitespaceCharacterSet = CharacterSet.whitespaces
-   
+        guard let strippedCityString = searchController.searchBar.text?.trimmingCharacters(in: whitespaceCharacterSet) else { return  }
+        // TODO: Add a check for internet connection here and everywhere else required
+        if(validateCity(searchQuery: strippedCityString)) {
+            print("City found!!")
+            userDefaults.set(strippedCityString, forKey: "last_location")
+            // TODO: add the WeatherModel instance as well in defaults. get that data if user is offline
+            fetchWeather(city: strippedCityString)
+        } else {
+            //TODO: Show error - only usa cities allowed
+            print("Error- wrong city!")
+        }
     }
     
-
+    func validateCity(searchQuery: String) -> Bool {
+        return citiesList.contains(where: { $0.cityName.lowercased() == searchQuery.lowercased() })
+    }
+    
+    func fetchWeather(city: String, unit: Unit = .imperial) {
+        viewModel.service.getWeather(city: city, unit: unit) { result in
+            DispatchQueue.main.async { [weak self] in
+                switch result {
+                case .success(let weather):
+                    print("Fetched weather")
+                    print(weather)
+                    self?.isWeatherFetched = true
+                    self?.viewModel = WeatherViewModel(weatherObj: weather)
+                    self?.setupView()
+                case .failure(let error):
+                    self?.isWeatherFetched = true
+                    print(error)
+                }
+            }
+        }
+    }
     
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         if manager.authorizationStatus == .authorizedWhenInUse {

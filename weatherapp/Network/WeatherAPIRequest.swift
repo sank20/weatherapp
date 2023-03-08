7d0fd16b61aa463ef85cbf629adeb9f90eeea9f2
Sanket Pimple <sanketpimple@gmail.com>
2023-03-08 00:06:56 -0800

    Refactor APIRequest

--- weatherapp/Network/WeatherAPIRequest.swift
+++ weatherapp/Network/WeatherAPIRequest.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 enum Unit: String {
     case imperial
@@ -14,20 +15,23 @@ enum Unit: String {
 }
 
 protocol WeatherRequest {
-    func getWeather(city: String, unit: Unit, completion: @escaping(Result<WeatherModel, APIError>) -> Void)
+    func getWeather(coordinate: CLLocationCoordinate2D, unit: Unit, completion: @escaping(Result<WeatherModel, APIError>) -> Void)
 }
 
 // in case of large number of request, abstract out all request protocols so that the service
 // class will have to conform to one protocol typealias
 class WeatherAPIRequest: WeatherRequest {
     
-    func getWeather(city: String, unit: Unit, completion: @escaping (Result<WeatherModel, APIError>) -> Void) {
+    private let apiKey = "d0d6ea9cd1097d3795ca6c8fe0c327cb"
+    
+    func getWeather(coordinate: CLLocationCoordinate2D, unit: Unit, completion: @escaping (Result<WeatherModel, APIError>) -> Void) {
         var url: URL {
             var components = URLComponents(string: "https://api.openweathermap.org/data/2.5/weather")!
             components.queryItems = [
-                URLQueryItem(name: "q", value: city),
+                URLQueryItem(name: "lat", value: coordinate.latitude.description),
+                 URLQueryItem(name: "lon", value: coordinate.longitude.description),
                 URLQueryItem(name: "units", value: unit.rawValue),
-                URLQueryItem(name: "appid", value: "d0d6ea9cd1097d3795ca6c8fe0c327cb") // TODO: store the APIKey in a secure place, like keychain
+                URLQueryItem(name: "appid", value: apiKey) // TODO: store the APIKey in a secure place, like keychain
             ]
             return components.url!
         }

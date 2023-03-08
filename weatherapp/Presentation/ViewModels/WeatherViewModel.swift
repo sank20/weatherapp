fafdb3c8ecd5b3a2844e21aa1226ae73225fea07
Sanket Pimple <sanketpimple@gmail.com>
2023-03-07 16:41:06 -0800

    Define viewModel

--- weatherapp/Presentation/ViewModels/WeatherViewModel.swift
+++ weatherapp/Presentation/ViewModels/WeatherViewModel.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 class WeatherViewModel {
-  
+    let service: WeatherAPIRequest
+    
+//    TODO: Define all the values to set directly to UILabels on the screen
+    //TODO: make object private if required
+    private(set) var weatherObj: WeatherModel
+    init(service: WeatherAPIRequest = WeatherAPIRequest(), weatherObj: WeatherModel = WeatherModel()) {
+        self.service = service
+        self.weatherObj = weatherObj
+    }
 }
